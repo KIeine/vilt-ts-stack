5b154859b935d0d8526d7151c1f3deac4f5becad
Kleine <renatasgal420@gmail.com>
2022-02-22 18:12:24 +0200

    add vue auto imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { fileURLToPath, URL } from 'url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      imports: ['vue'],
+    }),
+  ],
   optimizeDeps: {
     include: ['@inertiajs/inertia', '@inertiajs/inertia-vue3', 'vue'],
   },
