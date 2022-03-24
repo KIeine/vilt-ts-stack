6b4827ce3b51b024eb21de1c3aebb860f13f7843
Kleine <renatasgal420@gmail.com>
2022-03-24 17:38:49 +0200

    enable vue reactivityTransform 😍

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,9 @@ import AutoImport from 'unplugin-auto-import/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      reactivityTransform: true,
+    }),
     vueJsx(),
     AutoImport({
       imports: ['vue'],
